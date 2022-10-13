c179cee4d883f95c38e548dc95fe9ce10d7ad1ff
hanzhaoshuai <hanzhaoshuai@rhtect.com>
2022-10-13 17:07:23 +0800

    添加删除数据的接口

--- utils/pg_util.go
+++ utils/pg_util.go
@@ -39,6 +39,7 @@ type DataStroe struct {
 	Ip         string
 	Remarks    string
 	DataType   string
+	CreateName string
 	CreateTime time.Time
 	UpdateTime time.Time
 	Path       string
@@ -58,7 +59,7 @@ func DbOpen(dbConfig *dbConfig) (db *gorm.DB) {
 	// db, err = sql.Open("postgres", "host=192.168.10.79 port=5432 user=postgres password=pwd@123 dbname=ahdb sslmode=disable")
 	dc := ParseDbConfig()
 	// dsn := "host=192.168.10.68 user=postgres password=pwd@123 dbname=cluster port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=cluster port=5432 sslmode=disable TimeZone=Asia/Shanghai", dc.Cluster.Postgres.Ip, dc.Cluster.Postgres.Username, dc.Cluster.Postgres.Password)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai", dc.Cluster.Postgres.Ip, dc.Cluster.Postgres.Username, dc.Cluster.Postgres.Password, dc.Cluster.Postgres.DatasourceInfo.Schema, dc.Cluster.Postgres.Port)
 	var err error
 	if db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}); err == nil {
 		// fmt.Println("*************************connect to db success")
@@ -82,7 +83,7 @@ func DbOpen(dbConfig *dbConfig) (db *gorm.DB) {
 // 基于sql查询数据库信息
 func PgServiceQuery(db *gorm.DB) (servicePort []ServicePort) {
 	sps := make([]ServicePort, 0)
-	sql := ` SELECT dgc.id as id, gn.name as service_name, dgc.service_name as child_service, 
+	sql := fmt.Sprintf(` SELECT dgc.id as id, gn.name as service_name, dgc.service_name as child_service, 
 				-- case 
 				-- 	when dgc.remarks='' then '大数据融合平台'
 				-- else
@@ -98,27 +99,30 @@ func PgServiceQuery(db *gorm.DB) (servicePort []ServicePort) {
 				dgc.remarks as comment,
 				dgc.username as username,
 				dgc.password as password
-				FROM public.data_gather_configure dgc 
-				JOIN public.gather_name gn 
-				ON dgc.service_type=gn.id `
+				FROM public.%s dgc 
+				JOIN public.%s gn 
+				ON dgc.service_type=gn.id `, DbConfig.Cluster.Postgres.GatherDetailTable, DbConfig.Cluster.Postgres.GatherTable)
 	db.Raw(sql).Scan(&sps)
 	return sps
 }
 
 // 查询exporter地址数据
-func PgDataStoreQuery(db *gorm.DB) []DataStroe {
-	dss := make([]DataStroe, 0)
-	sql := ` SELECT dsc.id as id,
-			 dsc.data_name as dataname,
-			 dsc.ip as ip,
-			 dsc.remarks as remarks,
-			 dsc.data_type as datatype,
-			 dsc.create_name as create_name,
-			 dsc.create_time as create_time,
-			 dsc.update_time as update_time,
-			 dsc.path as path
-			 FROM PUBLIC.data_store_configure dsc`
-	db.Raw(sql).Scan(&dss)
+func PgDataStoreQuery(db *gorm.DB, table string) []Data_store_configure_default {
+	dss := make([]Data_store_configure_default, 0)
+	// sql := fmt.Sprintf(` SELECT dsc.id as id,
+	// 		dsc.data_name as dataname,
+	// 		dsc.ip as ip,
+	// 		dsc.remarks as remarks,
+	// 		dsc.data_type as datatype,
+	// 		dsc.create_name as createname,
+	// 		dsc.create_time as createtime,
+	// 		dsc.update_time as updatetime,
+	// 		dsc.path as path
+	// 		FROM PUBLIC.%s dsc`, table) //data_store_configure
+	// fmt.Println("sql: ", sql)
+	// db.Raw(sql).Scan(&dss)
+	// 使用gorm语法
+	db.Find(&dss)
 	return dss
 
 }
@@ -128,15 +132,19 @@ func PgDataStoreInsert(db *gorm.DB, datas *Data_store_configure_default) {
 	db = db.Create(datas)
 }
 
+func PgDataStoreRemove(db *gorm.DB) {
+	db.Where("1=1").Delete(&Data_store_configure_default{})
+}
+
 // 查询基础服务存活数据的个数
 func PgCountQuery(db *gorm.DB, sql string) int {
 	if sql == "" {
-		sql = ` SELECT COUNT(*) 
+		sql = fmt.Sprintf(` SELECT COUNT(*) 
 		FROM 
 		(
 			SELECT DISTINCT service_name, ip, port, service_type 
-			FROM  data_gather_configure
-		) tmp`
+			FROM  %s
+		) tmp`, DbConfig.Cluster.Postgres.GatherDetailTable)
 	}
 	var countValue int
 	db.Raw(sql).Scan(&countValue)
@@ -145,13 +153,15 @@ func PgCountQuery(db *gorm.DB, sql string) int {
 
 // pg数据库中插入数据
 func PgGatherNameInsert(db *gorm.DB, srcData gatherName) {
-	Logger.Printf("插入表gather_name数据:  %v \n", srcData)
-	db.Table("public.gather_name").Create(&srcData)
+	Logger.Printf("插入表%s数据:  %v \n", DbConfig.Cluster.Postgres.GatherTable, srcData)
+	db.Table(fmt.Sprintf("public.%s", DbConfig.Cluster.Postgres.GatherTable)).Create(&srcData)
 }
 
 func PgGatherNameConfigureInsert(db *gorm.DB, srcData dataGather) {
-	Logger.Printf("插入表data_gather_configure数据: %v ", srcData)
-	db.Table("public.data_gather_configure").Create(&srcData)
+
+	tableName := fmt.Sprintf("public.%s", DbConfig.Cluster.Postgres.GatherDetailTable)
+	Logger.Printf("插入表%s数据: %v ", tableName, srcData)
+	db.Table(tableName).Create(&srcData)
 }
 
 // 临时迁移数据从mysql到pg
@@ -206,12 +216,12 @@ func Migirate() {
 		// 插入gather_name表数据
 		// db.Table("gather_name")
 		PgGatherNameInsert(db, nameInfo)
-		Logger.Printf("插入gather_name数据: %v\n", nameInfo)
+		Logger.Printf("插入%s数据: %v\n", DbConfig.Cluster.Postgres.GatherTable, nameInfo)
 	}
 
 	for _, gatherInfo := range dataGathers {
 		// 插入data_gather_configure表数据
 		PgGatherNameConfigureInsert(db, gatherInfo)
-		Logger.Printf("插入gather_name_configure数据: %v\n", gatherInfo)
+		Logger.Printf("插入%s数据: %v\n", DbConfig.Cluster.Postgres.GatherDetailTable, gatherInfo)
 	}
 }
